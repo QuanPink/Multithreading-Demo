ee56225768354e0cccd2757975198c663b61860a
QuanPink <quanhoangkk01@gmail.com>
2023-12-15 15:16:53 +0700

    push something

--- src/test/java/setup/BrowserRunnable.java
+++ src/test/java/setup/BrowserRunnable.java
@@ -7,16 +7,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.concurrent.Semaphore;
+
 public class BrowserRunnable implements Runnable {
-    private final int tabLimit;
+    private final Semaphore semaphore;
     private final int width;
     private final int height;
     private final int position;
     private final int row;
-    private int openedTabs = 0;
 
-    public BrowserRunnable(int tabLimit, int width, int height, int position, int row) {
-        this.tabLimit = tabLimit;
+
+    public BrowserRunnable(Semaphore semaphore, int width, int height, int position, int row) {
+        this.semaphore = semaphore;
         this.width = width;
         this.height = height;
         this.position = position;
@@ -25,37 +27,38 @@ public class BrowserRunnable implements Runnable {
 
     @Override
     public void run() {
-        // Open tabs until limit is reached
-        while (openedTabs < tabLimit) {
-            WebDriver driver = null;
-            try {
-                // Install and initialize WebDriver
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+        try {
+            while (true) {
+                // Acquire a permit before opening a new browser
+                semaphore.acquire();
 
-                // Set the size and position of the window
-                driver.manage().window().setSize(new org.openqa.selenium.Dimension(width, height));
-                driver.manage().window().setPosition(new org.openqa.selenium.Point(position, row));
+                WebDriver driver = null;
+                try {
+                    // Install and initialize WebDriver
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    // Set the size and position of the window
+                    driver.manage().window().setSize(new org.openqa.selenium.Dimension(width, height));
+                    driver.manage().window().setPosition(new org.openqa.selenium.Point(position, row));
 
+                    driver.get("https://www.google.com/?hl=vi");
+                    driver.findElement(By.xpath("//textarea[@type = 'search']")).sendKeys("cháo");
+                    Actions action = new Actions(driver);
 
-                driver.get("https://www.google.com/?hl=vi");
-                driver.findElement(By.xpath("//textarea[@type = 'search']")).sendKeys("cháo");
-
-                Actions action = new Actions(driver);
-
-                action.sendKeys(Keys.ENTER).build().perform();
-                driver.findElement(By.xpath("(//h3)[1]")).click();
-
-                // Increase the number of opened tabs
-                openedTabs++;
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                // Make sure WebDriver is turned off upon completion
-                if (driver != null) {
-                    driver.quit();
+                    action.sendKeys(Keys.ENTER).build().perform();
+                    driver.findElement(By.xpath("(//h3)[1]")).click();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    // Make sure WebDriver is turned off upon completion
+                    if (driver != null) {
+                        driver.quit();
+                        semaphore.release();
+                    }
                 }
             }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
